447c695bf381019032f29433f2b30ed02ab6c218
AmanFatima19 <amanfatima713@gmail.com>
2025-03-05 08:37:43 +0500

    updating

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -7,8 +7,9 @@ function HeroSection() {
       <section class="back body-font py-6">
   <div class="container mx-auto flex py-6 md:flex-row flex-col items-center">
     <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-start">
-    <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-black">YOUR PETS ARE
-        <br class="hidden lg:inline-block text-[#F2C119]"/>OUR PRIORITY
+    <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium">YOUR PETS ARE
+        <br class="hidden lg:inline-block"/>
+        <span style={{color:'purple'}}>OUR PRIORITY</span>
       </h1>
 
       <p class="mb-8 leading-relaxed">Connecting loving homes with pets in need, our platform simplifies the adoption process with a user-friendly interface, detailed pet profiles, and secure management. Discover, adopt, and provide a better life for your future furry companions.</p>
